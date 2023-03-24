cc209062793e498d90cd28adcf4b31d185a7a012
tan <dinovu285@gmail.com>
2023-03-24 11:04:00 +0700

    .

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const handlebars = require('express-handlebars')
+const port = 3000
+
+//Templete engine
+app.engine('hbs', handlebars.engine({
+    extname: '.hbs'
+}));
+app.set('view engine','hbs')
+app.set('views', path.join(__dirname, 'resources/views'));
+console.log(path.join(__dirname, 'resources/views'))
+
+//HTTP Logger
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+// 127.0.0.1 - localhost
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
