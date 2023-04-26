4c7e43c67231ecd98766db2c94c780919e27d481
tan <dinovu285@gmail.com>
2023-04-26 19:42:58 +0700

    .

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,9 @@
-const petRouter = require('./pet');
+
 const siteRouter = require('./site');
 const productRouter = require('./products');
 const adRouter = require('./ad');
 
 function routes(app) {
-    app.use('/pet', petRouter);
     app.use('/products', productRouter);
     app.use('/ad', adRouter);
     app.use('/', siteRouter);
