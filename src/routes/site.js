743a0a81f933fc0936f2dca5b7ba6202188616e7
tan <dinovu285@gmail.com>
2023-04-26 19:53:44 +0700

    .

--- src/routes/site.js
+++ src/routes/site.js
@@ -5,7 +5,6 @@ const siteController = require('../app/controllers/siteController');
 
 siteController.index;
 
-router.get('/search', siteController.search);
 router.get('/', siteController.home);
 
 module.exports = router;
